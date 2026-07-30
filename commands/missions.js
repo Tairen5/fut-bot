7de950a4f848a1e14442b999d540e87124d67aed
JaviMC50 <pepeviyuelarito@gmail.com>
2026-07-30 23:44:34 +0100

    Use underscores for solid bottom line in missions UI

--- commands/missions.js
+++ commands/missions.js
@@ -79,7 +79,7 @@ export async function execute(interaction) {
 
       fields.push({
         name: `${icon}  ${obj.name.toUpperCase()}${statusSuffix}`,
-        value: `${obj.description}\n${bar}  **${progressText}**\n${rewardText}\n${'\u25ac'.repeat(12)}`,
+        value: `${obj.description}\n----------------------\n${bar}  **${progressText}**\n${rewardText}\n______________________`,
         inline: true
       });
 
